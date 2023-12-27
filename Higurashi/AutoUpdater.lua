33075240fbbb57f878db5cde46a9fa1ad71e87d1
ImHigurashi <132133042+ImHigurashi@users.noreply.github.com>
2023-12-27 20:47:05 +0900

    Update AutoUpdater.lua

--- Higurashi/AutoUpdater.lua
+++ Higurashi/AutoUpdater.lua
@@ -9,9 +9,9 @@ local file_paths = {
 }
 
 local files = {
-    main = [[https://raw.githubusercontent.com/ImHigurashi/dev/main/dev.lua]],
-    --changelog = [[ https://raw.githubusercontent.com/ImHigurashi/dev/main/Higurashi/Changelog.md ]],
-    --version = [[ https://raw.githubusercontent.com/ImHigurashi/dev/main/Higurashi/Version.md ]],
+    main = [[https://raw.githubusercontent.com/ImHigurashi/dev/main/dev.lua ]],
+    changelog = [[https://raw.githubusercontent.com/ImHigurashi/dev/main/Higurashi/Changelog.md ]],
+    version = [[https://raw.githubusercontent.com/ImHigurashi/dev/main/Higurashi/Version.md ]],
 }
 
 local all_files = 0
